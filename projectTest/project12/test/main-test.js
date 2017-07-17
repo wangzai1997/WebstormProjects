ea7479be83029ab482175c9c7ce20d9928e1be0c
卢旺 <1043846926@qq.com>
2017-07-17 17:31:49 +0800

    Add files via upload

---
+++ projectTest/project12/test/main-test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+describe("00入门",function () {
+    it("写一个类—1",function () {
+        var result = new Person("Tom",21).introduce();
+        var expect_string = 'My name is Tom. I am 21 years old.';
+
+        expect(expect_string).toEqual(result);
+
+        });
+
+    it("写一个类—2",function () {
+        var class1;
+        class1 = new Class;
+        class1.which = 3;
+        class1.assignLeader = 'Tom';
+        var result = new Student("Tom",21,class1).introduce();
+        var expect_string = 'My name is Tom. I am 21 years old. I am a Student. I am Leader of Class 3.';
+
+        expect(expect_string).toEqual(result);
+
+    });
+
+    it("写一个类—3",function () {
+        var result = new Teacher("Tom",21,3).introduce();
+        var expect_string = 'My name is Tom. I am 21 years old. I am a Teacher. I teach Class 3.';
+
+        expect(expect_string).toEqual(result);
+
+    });
+
+
+
+
+});
